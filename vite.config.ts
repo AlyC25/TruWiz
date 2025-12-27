4b378f07906a9106821a79cc4be4dbf80469d1c4
Chakradhar Guntuboina <47364794+ChakradharG@users.noreply.github.com>
2025-12-27 09:45:03 +0530

    Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: "/TruWiz",  
-server: {
+  base: "/TruWiz/",
+  server: {
     host: "::",
     port: 8080,
   },
